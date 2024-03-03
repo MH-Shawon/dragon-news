29f13e4b3bfb6a4c0ad4817dfe7efd174501a634
Mohsin <mohsin@localhost.localdomain>
2024-03-03 16:11:11 +0600

    final deploy

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
+  apiKey: "AIzaSyBWXKQfoUMGnO4sw49DDyxcwUL2SwTnN84",
+  authDomain: "dragon-news-3d21b.firebaseapp.com",
+  projectId: "dragon-news-3d21b",
+  storageBucket: "dragon-news-3d21b.appspot.com",
+  messagingSenderId: "39740240229",
+  appId: "1:39740240229:web:14ced85733d01eb9e2b18c",
 };
 
 // Initialize Firebase
